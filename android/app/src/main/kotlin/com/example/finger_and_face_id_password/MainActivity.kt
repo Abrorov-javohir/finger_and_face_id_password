95fe7dbd378d983fcea3e1c5d72deed1775ce408
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-08-10 08:53:02 +0500

    homeworkd222222

---
+++ android/app/src/main/kotlin/com/example/finger_and_face_id_password/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finger_and_face_id_password
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:finger_and_face_id_password/bloc/game_cubit.dart';
+import 'package:finger_and_face_id_password/screens/bio_metric_auth_page.dart';
+import 'package:finger_and_face_id_password/screens/game_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<GameCubit>(
+          create: (_) => GameCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        home: AuthPage(),
+      ),
+    );
+  }
+}
